d57c52811b7d8eb62c04c7ece44e5a1a42417ad6
Hasnain <hkousar@DellKousar.>
2025-06-04 17:14:49 -0400

    added comments and docstring to main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+"""
+Entry point for the calculator application.
+
+It initializes and runs the calculator app.
+"""
+
 from app.calculator import calculator
 
-
+# Ensure the calculator runs only when this script is executed directly.
 if __name__ == "__main__":
 
     calculator()
