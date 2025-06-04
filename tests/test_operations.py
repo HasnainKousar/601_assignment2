1554b1e992a52f8c4040ae250040c704f160d76d
Hasnain <hkousar@DellKousar.>
2025-06-04 12:52:03 -0400

    added rest of the test casses for all operations functions

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -45,3 +45,62 @@ def test_divide_positive():
     assert divide(-12, -3) == 4.0  
 
 
+
+#negative tests for add, subtract, multiply and divide functions
+def test_add_negative():
+    """
+    The function will test postive casses for add function.
+    Assert statement will validate test conditions. If the codition evaluates to True, the test passes, otherwise it fails. 
+    """
+    assert add(3.0,-4.0) == -1.0
+    assert add(2,-7) == -5.0
+    assert add(-1, -5) == -6.0
+
+
+
+def test_subtract_negative():
+    """
+    The function will test postive casses for the subtract function.
+    Assert statement will validate test conditions. If the codition evaluates to True, the test passes, otherwise it fails. 
+    """
+    assert subtract(4, 8) == -4.0
+    assert subtract(3, 6.0) == -3.0
+    assert subtract(-6, -2) == -4.0   
+
+
+
+def test_multiply_negative():
+    """
+    The function will test postive casses for the multiply function.
+    Assert statement will validate test conditions. If the codition evaluates to True, the test passes, otherwise it fails. 
+    """
+    assert multiply(3, -6) == -18.0
+    assert multiply(-5.0, 2.0) == -10.0
+    assert multiply(-4, 4) == -16.0   
+
+
+
+def test_divide_negative():
+    """
+    The function will test postive casses for divide function.
+    Assert statement will validate test conditions. If the codition evaluates to True, the test passes, otherwise it fails. 
+    """
+    assert divide(8, -4) == -2.0
+    assert divide(9.0, -3.0) == -3.0
+    assert divide(-12, 3) == -4.0  
+
+
+# Test for divide function in the case of division by zero
+def test_divide_by_zero():
+    """
+    Test that the divide function raises a ValueError when attempting 
+    to divide by zero.
+
+    This test verifies that dividing by zero correctly triggers a 
+    ValueError with the expected error message.
+    """
+    with pytest.raises(ValueError, match="Division by Zero is not allowed."):
+        divide(10, 0)
+
+
+
