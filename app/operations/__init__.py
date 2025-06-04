e2b7ac540d166ce32e545078f9cec6f9c77cfc2d
Hasnain <hkousar@DellKousar.>
2025-06-04 12:03:00 -0400

    added the function for addition

---
+++ app/operations/__init__.py
@@ -0,0 +1,12 @@
+#operations.py
+#write functions for add, subtract, multiply and divide
+
+def add(n1: float, n2: float) -> float:
+    """
+    The add function will take two float values n1 and n2, and returns their sum (n1 + n2).
+    float values means n1 and n2 are supposed to numbers with decimal points, and the result will also be a number with decimal points
+    Example: if we call the function add(2.0, 7.0), it will return 9.0. 
+    """
+    return (n1+n2) #this is the math part where it will add n1 and n2 and return their sum
+
+
